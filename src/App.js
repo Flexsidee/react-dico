04bc9223fa16594fdc85b4601c75a01c25806e49
Somade Daniel <Flexsidee@gmail.com>
2021-09-06 22:49:00 +0100

    latest update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import Header from "./components/header/Header";
+import axios from "axios";
+import { Container } from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/plane"
+      );
+      setMeanings(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(meanings);
+
+  useEffect(() => {
+    dictionaryApi();
+  }, []);
+
   return (
-    <div className="App">
-     hello
+    <div
+      className="App"
+      style={{ background: "#282c34", height: "100vh", color: "white" }}
+    >
+      <Container
+        maxWidth="md"
+        style={{ height: "100vh", display: "flex", flexDirection: "column" }}
+      >
+        <Header />
+      </Container>
     </div>
   );
 }
